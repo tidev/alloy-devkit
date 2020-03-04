047575b72ecaf15ca7b73cd19cd756d8caf14fb9
Jan Vennemann <jan.vennemann@gmx.net>
2020-03-04 15:39:19 +0100

    fix(compiler): remove unused babel plugin

--- packages/alloy-compiler/lib/compilers/utils.js
+++ packages/alloy-compiler/lib/compilers/utils.js
@@ -1,7 +1,6 @@
 function configureBabelPlugins(compileConfig) {
 	return [
 		[ require('../ast/builtins-plugin'), compileConfig ],
-		[ require('../ast/handle-alloy-globals') ],
 		[ require('../ast/optimizer-plugin'), compileConfig.alloyConfig ],
 	];
 }
