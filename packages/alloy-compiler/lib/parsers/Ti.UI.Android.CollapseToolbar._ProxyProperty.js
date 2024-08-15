25807928aac25bb47b75fa46abbe109dc53b9ace
Jan Vennemann <j.vennemann@lambus.com>
2024-08-15 18:01:23 +0200

    chore: update parsers

---
+++ packages/alloy-compiler/lib/parsers/Ti.UI.Android.CollapseToolbar._ProxyProperty.js
@@ -0,0 +1,10 @@
+const _ = require('lodash');
+
+exports.parse = function (node, state) {
+	_.extend(state, {
+		proxyPropertyDefinition: {
+			parents: ['Ti.UI.Android.CollapseToolbar', 'Ti.UI.iPad.Popover'],
+		},
+	});
+	return require('./Alloy.Abstract._ProxyProperty').parse(node, state);
+};
